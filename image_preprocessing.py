1b2e235113f531929d18ad383b996583fc52ae23
ccrenfroe <caleb.renfroe007@gmail.com>
2020-07-08 13:23:11 -0400

    First implementation of image_processing.py complete. Added 1 image to the TestingImages folder.

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,33 +1,35 @@
 # This script will handle all of the preprocessing of images I need to do before I can feed it into the Birds model for image classification.
 
 #Imports
+import sys
 from PIL import Image
 from numpy import asarray
 
 def process_image(image_path):
+    min = sys.maxsize
+    max = -sys.maxsize
+
+
     image = Image.open(image_path)
-    new_image = image.resize((224,224))
-    
-    pixels = asarray(new_image)
+    new_image = image.resize((224,224))    
+    np_image = asarray(new_image)
 
-    # confirm pixel range is 0-255
-    print('Data Type: %s' % pixels.dtype)
-    print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+    if min > np_image.min():
+        min = np_image.min()
+    if max < np_image.max():
+        max = np_image.max()    
 
-    # convert from integers to floats
-    pixels = pixels.astype('float32')
+    np_image = np_image.astype('float32')
+    np_image -= min
+    np_image /= (max - min)
+    # print(np_image)
+    img = Image.fromarray(np_image, 'RGB')
+    img.show()
 
-    # normalize to the range 0-1
-    pixels /= 255.0
-
-    # confirm the normalization
-    print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
-    new_image.save("result.jpg")
-    
-    return new_image
+    return img
 
 def main():
-    image = "Images/TestImages/test.jpg"
+    image = "Images/TestImages/mourningdove.jpg"
     new_image = process_image(image)
 
     return
