0682c1e36ce53085dc24da4cf0748c3432acd9da
ccrenfroe <caleb.renfroe007@gmail.com>
2020-07-15 13:28:09 -0400

    Added Preprocessing & PicturesIn directories

--- inference.py
+++ inference.py
@@ -1,23 +1,22 @@
 import argparse
 import numpy as np
-import tflite_runtime.interpreter as tflite
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-# Load interpreter
-interpreter = tflite.Interpreter(model_path="aiy_vision_classifier_birds_V1_2.tflite")
-interpreter.allocate_tensors()
+# import tflite_runtime.interpreter as tflite //Why was this here?
+import tensorflow as tf
 
+# Load interpreter
+interpreter = tf.lite.Interpreter(model_path="aiy_vision_classifier_birds_V1_2.tflite")
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
+
+interpreter.allocate_tensors()
 
 print(input_details)
 print(output_details)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-            'i',
-            '--image',
-            help='image to be classified')
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#             'i',
+#             '--image',
+#             help='image to be classified')
