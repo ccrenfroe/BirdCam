1f45c1ec8259e0648a5e22a745d0646c60798bc7
ccrenfroe <caleb.renfroe007@gmail.com>
2020-07-15 14:40:02 -0400

    Extended functionality to iterate over directory.

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,37 +1,47 @@
 # This script will handle all of the preprocessing of images I need to do before I can feed it into the Birds model for image classification.
-
 #Imports
+import os
 import sys
 from PIL import Image
 from numpy import asarray
 
-def process_image(image_path):
-    min = sys.maxsize
-    max = -sys.maxsize
+DIRECTORY_IN = 'Images/PicturesIn/' # Folder of images to be processed
+DIRECTORY_OUT = 'Images/Processed/' # Folder for images post processing
 
+def process_image(DIRECTORY_IN):
+    for filename in os.listdir(DIRECTORY_IN):
+        # Used to find the min and max of the image file
+        # min = sys.maxsize
+        # max = -sys.maxsize
+        filepath = DIRECTORY_IN + filename
+    
+        image = Image.open(filepath)
+        new_image = image.resize((224,224))    
+        np_image = asarray(new_image)
 
-    image = Image.open(image_path)
-    new_image = image.resize((224,224))    
-    np_image = asarray(new_image)
+        # Performing minmax normalization
+        # if min > np_image.min():
+        min = np_image.min() # Get min
+        print(min)
+        # if max < np_image.max():
+        max = np_image.max() # Get max
+        print(max)
+        np_image = np_image.astype('float32')
+        print('Min: %.3f, Max: %.3f' % (np_image.min(), np_image.max()))
+        np_image -= min
+        np_image /= (max - min)
+        print('Min: %.3f, Max: %.3f' % (np_image.min(), np_image.max()))
+        # print(np_image)
+        img = Image.fromarray(np_image, 'RGB') # Convert back to an image
 
-    if min > np_image.min():
-        min = np_image.min()
-    if max < np_image.max():
-        max = np_image.max()    
-
-    np_image = np_image.astype('float32')
-    np_image -= min
-    np_image /= (max - min)
-    # print(np_image)
-    img = Image.fromarray(np_image, 'RGB')
-    img.show()
-
-    return img
+        # Save to 'Processed' directory
+        filename = filename.split(".")[0] + "_processed.png"
+        print(filename)
+        img.save(DIRECTORY_OUT + filename)
+    return
 
 def main():
-    image = "Images/TestImages/mourningdove.jpg"
-    new_image = process_image(image)
-
+    process_image(DIRECTORY_IN)
     return
 
 if __name__ == "__main__":
