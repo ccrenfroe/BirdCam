d88b95384fba8e45bec033b23151f265f6c24de9
ccrenfroe <caleb.renfroe007@gmail.com>
2020-07-22 16:37:08 -0400

    Completed comments on inference.py

--- inference.py
+++ inference.py
@@ -1,14 +1,14 @@
-import argparse
+# The purpose of this script is to identify birds in images fed in. It includes all of the functions necessary to assist in the process of inference on images.
 import numpy as np
 from numpy import asarray
 import csv
 import tensorflow as tf
-#For image preprocessing
 import os
 import sys
 from PIL import Image
 
 def process_image(image_path):
+    """Preprocess the image to be put into the model"""
     image = Image.open(image_path)
     new_image = image.resize((224,224))
     np_image = asarray(new_image)
@@ -16,45 +16,53 @@ def process_image(image_path):
     return [np_image]
 
 def find_label(model_output):
+    """Return the label corresponding to the bird based off of the inference results"""
     max_label = 0
     index  = -1
-    for label in model_output:
+    # Loop through model_output array and find the largest value
+    for label in model_output: 
         index += 1
         if max_label < label:
             answer = index
     return answer
 
 def get_bird_name(index):
+    """Return birds name according to labelmap"""
     with open('aiy_birds_V1_labelmap.csv') as csvIn:
-        labels = list(csv.reader(csvIn))
-        label = labels[index+2][1]
+        labels = list(csv.reader(csvIn)) # Converts the labelmap.csv to a list for easy indexing.
+        label = labels[index+2][1] # Plus 2 to ignore the first 2 lines of the CSV, and 1 to go to the 'name' column
     return label
 
-# Load interpreter
-interpreter = tf.lite.Interpreter(model_path="aiy_vision_classifier_birds_V1_2.tflite")
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+def classify_bird(image_in):
+    """
+    Returns the bird detected in the image
+    """
+    # Load interpreter
+    interpreter = tf.lite.Interpreter(model_path="aiy_vision_classifier_birds_V1_2.tflite")
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
 
-interpreter.allocate_tensors()
+    interpreter.allocate_tensors()
 
-# Inputting the image
-image = process_image('Images/PicturesIn/painted-bunting_adult-male.jpg')
-interpreter.set_tensor(input_details[0]['index'], image)
+    # Inputting the image
+    image = process_image(image_in)
+    interpreter.set_tensor(input_details[0]['index'], image)
 
-# run the inference
-interpreter.invoke()
+    # run the inference
+    interpreter.invoke()
 
-output_data = interpreter.get_tensor(output_details[0]['index'])[0]
+    output_data = interpreter.get_tensor(output_details[0]['index'])[0]
 
-# Output
-index = find_label(output_data)
-label = get_bird_name(index)
-print(label)
+    # Output
+    index = find_label(output_data)
+    label = get_bird_name(index)
+    print(label)
+    return label
 
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument(
-#             'i',
-#             '--image',
-#             help='image to be classified')
+def main():
+    image = "Images/PicturesIn/painted-bunting_adult-male.jpg"
+    classify_bird(image)
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,9 @@
+# Thinking this will be for the future. 1  main script to run everything from.
+
+# import argparse
+# if __name__ == '__main__':
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument(
+#             'i',
+#             '--image',
+#             help='image to be classified')
