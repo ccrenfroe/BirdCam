dcddd34c6a67d7dd807b50af1031a98bcc9601fb
ccrenfroe <caleb.renfroe007@gmail.com>
2020-07-23 14:33:49 -0400

    moved main outside of the birdcam package

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import BirdCam.inference as inferencer
+
+def main():
+    image = "Images/PicturesIn/blue_Jay.jpg"
+    inferencer.classify_bird(image)
+
+if __name__ == "__main__":
+    main()
+
 # Thinking this will be for the future. 1  main script to run everything from.
 
 # import argparse
